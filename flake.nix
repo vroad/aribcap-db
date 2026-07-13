34511cdbd62aaa71f2d21187bacba5079bcb06b1
vroad <396351+vroad@users.noreply.github.com>
2026-07-13 14:39:04 +0000

    chore: initial commit

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "aribcap-db Rust development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            cargo
+            clippy
+            nixpkgs-fmt
+            pre-commit
+            rust-analyzer
+            rustc
+            rustfmt
+          ];
+        };
+      });
+}
---
+++ src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    aribcap_db::run();
+}
